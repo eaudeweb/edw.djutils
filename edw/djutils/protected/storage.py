d63d67f302dec4987b8462c335ea104bde8ca05a
Ionuț Ciocîrlan <ionut.ciocirlan@eaudeweb.ro>
2015-12-18 14:07:33 +0200

    simplified private storage

--- edw/djutils/protected/storage.py
+++ edw/djutils/protected/storage.py
@@ -2,13 +2,5 @@ from django.conf import settings
 from django.core.files.storage import get_storage_class
 
 
-def get_storage_instance(**kwargs):
-    """
-    Returns an instance of the default storage with the given kwargs.
-    """
-    cls = get_storage_class()
-    return cls(**kwargs)
-
-
-private_storage = get_storage_instance(location=settings.PRIVATE_ROOT,
-                                       base_url=settings.PRIVATE_URL)
+private_storage = get_storage_class()(location=settings.PRIVATE_ROOT,
+                                      base_url=settings.PRIVATE_URL)
